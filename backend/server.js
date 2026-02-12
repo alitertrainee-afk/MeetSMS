97c91d75f04f7e387ea413208e28b445fbb479d7
Meet Chauhan <alitertrainee@gmail.com>
2026-02-12 15:06:07 +0530

    [bug:backend]:[fixed import routes after moving to src folders]

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,15 @@
 
 
 // local imports
-import { connect } from "./database/db.js";
-import app from "./app/app.js";
-import { logger } from "./config/logger.js";
-import { env } from "./config/env.js";
+import { connect } from "./src/database/db.js";
+import app from "./src/app/app.js";
+import { logger } from "./src/config/logger.js";
+import { env } from "./src/config/env.js";
 
 connect()
   .then(() => {
     app.listen(env.PORT, () => {
-      logger.info(`server listen on port${env.PORT}`);
+      logger.info(`server listen on port ${env.PORT}`);
     });
   })
   .catch((error) => {
